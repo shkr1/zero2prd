ca7cfe46ba98ab96080ef3f7d75745fed078bea4
Gildardo Rosas <gildardo.rosas96@gmail.com>
2023-01-21 14:00:59 -0800

    restructure project
    added sqlx dependency
    changed project structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prd::startup::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Faild to bind port");
+    run(listener)?.await
 }
