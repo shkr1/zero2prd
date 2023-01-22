21a310f7917e3b33c18597189bc4a0f6c68d04cb
Gildardo Rosas <gildardo.rosas96@gmail.com>
2023-01-21 16:45:10 -0800

    add telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::net::TcpListener;
 
 use sqlx::PgPool;
+use zero2prd::telemetry::{get_subscriber, init_subscriber};
 use zero2prd::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prd".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Faild to bind port");
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,7 +4,22 @@ use sqlx::{Connection, Executor, PgConnection, PgPool};
 use zero2prd::configuration::{get_configuration, DatabaseSettings};
 use zero2prd::startup::run;
 
+use once_cell::sync::Lazy;
 use uuid::Uuid;
+use zero2prd::telemetry::{get_subscriber, init_subscriber};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 pub struct TestApp {
     pub address: String,
@@ -12,6 +27,8 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
